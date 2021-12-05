4e9b6a4a4199124e6db0bc5fc6f323f50ca41fdc
Pavel Žák <pavel@zak.global>
2021-12-05 05:21:57 +0100

    Create expedition staff models
    Relates to #12, #13

--- packages/fantasion-backend/fantasion_expeditions/translation.py
+++ packages/fantasion-backend/fantasion_expeditions/translation.py
@@ -8,6 +8,11 @@ class AgeGroupTranslationOptions(TranslationOptions):
     fields = ('title',)
 
 
+@register(models.StaffRole)
+class StaffRoleTranslationOptions(TranslationOptions):
+    fields = ('title', 'description',)
+
+
 @register(models.LeisureCentre)
 class LeisureCentreTranslationOptions(TranslationOptions):
     fields = ('title', 'description',)
