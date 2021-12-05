5ecda37d761df9432b6877b53917b435a0acae77
Pavel Žák <pavel@zak.global>
2021-12-05 03:14:00 +0100

    Generate UUID for each media file

--- packages/fantasion-backend/fantasion_generics/upload_path.py
+++ packages/fantasion-backend/fantasion_generics/upload_path.py
@@ -1,5 +1,14 @@
+import os
+
+from uuid import uuid4
+from datetime import datetime
+
 def get_upload_path(instance, filename):
-    return '{0}/%Y/%m/%d/{1}'.format(
-        instance.get_upload_dir(),
-        filename
+    date = datetime.today()
+    name, extension = os.path.splitext(filename)
+    return '{base_dir}/{date}-{uuid}.{extension}'.format(
+        base_dir=instance.get_upload_dir(),
+        date=date.strftime('%Y-%m-%d'),
+        uuid=uuid4().hex,
+        extension=extension,
     )
