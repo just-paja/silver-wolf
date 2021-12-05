5f77402b0907edbd1ae358eff752d6b6e67d1e7f
Pavel Žák <pavel@zak.global>
2021-12-05 05:11:15 +0100

    Define family models
    Fixes #11

--- packages/fantasion-backend/fantasion_people/models.py
+++ packages/fantasion-backend/fantasion_people/models.py
@@ -1,8 +1,17 @@
 from django.utils.translation import ugettext_lazy as _
-from django.db.models import BooleanField, ForeignKey, CASCADE, SET_DEFAULT
 from django.contrib.auth.models import User
+from django_extensions.db.models import TimeStampedModel
+from django.db.models import (
+    BooleanField,
+    ForeignKey,
+    PositiveIntegerField,
+    CASCADE,
+    RESTRICT,
+    SET_DEFAULT,
+)
 
 from fantasion_generics.models import MediaObjectModel, NamedModel, PublicModel
+from fantasion_generics.titles import TitleField
 
 
 class Profile(PublicModel):
@@ -35,3 +44,42 @@ class Allergy(NamedModel):
 
 class Sport(NamedModel):
     pass
+
+
+class Family(TimeStampedModel):
+    title = TitleField()
+    owner = ForeignKey(
+        User,
+        on_delete=RESTRICT,
+        related_name='families',
+    )
+
+# Admin can assign family roles
+FAMILY_ROLE_ADMIN = 1
+# Representative can submit signups
+FAMILY_ROLE_REPRESENTATIVE = 2
+# Spectator can view content
+FAMILY_ROLE_SPECTATOR = 3
+
+FAMILY_ROLE_CHOICES = (
+    (FAMILY_ROLE_ADMIN, _('Administrator')),
+    (FAMILY_ROLE_REPRESENTATIVE, _('Legal representative')),
+    (FAMILY_ROLE_SPECTATOR, _('Spectator')),
+)
+
+
+class FamilyMember(TimeStampedModel):
+    family = ForeignKey(
+        Family,
+        on_delete=CASCADE,
+        related_name='members',
+    )
+    user = ForeignKey(
+        User,
+        on_delete=CASCADE,
+        related_name='family_members',
+    )
+    role = PositiveIntegerField(
+        choices=FAMILY_ROLE_CHOICES,
+        default=FAMILY_ROLE_SPECTATOR,
+    )
