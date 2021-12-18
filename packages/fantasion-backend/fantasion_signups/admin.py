c54a8c64357369bac5eb96dd7db59e3ea0e8a929
Pavel Žák <pavel@zak.global>
2021-12-18 23:11:00 +0100

    Filter signups
    Fixes #15

--- packages/fantasion-backend/fantasion_signups/admin.py
+++ packages/fantasion-backend/fantasion_signups/admin.py
@@ -9,3 +9,31 @@ class ParticipantAdmin(BaseAdmin):
 
 class SignupAdmin(BaseAdmin):
     model = models.Signup
+    list_display = (
+        'participant_name',
+        'status',
+        'expedition',
+        'date_range',
+        'age_group',
+    )
+    list_filter = (
+        'batch_age_group__batch__expedition',
+        'batch_age_group__batch',
+        'batch_age_group__age_group',
+        'status',
+    )
+
+    def age_group(self, inst):
+        return inst.batch_age_group.age_group
+
+    def date_range(self, inst):
+        return '{starts_at} - {ends_at}'.format(
+            starts_at=inst.batch_age_group.batch.starts_at,
+            ends_at=inst.batch_age_group.batch.ends_at,
+        )
+
+    def expedition(self, inst):
+        return inst.batch_age_group.batch.expedition
+
+    def participant_name(self, inst):
+        return inst.participant.name
