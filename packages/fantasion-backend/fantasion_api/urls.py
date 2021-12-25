6e51ac64775f079871a67a1dfbcf2ff44df4cdfa
Pavel Žák <pavel@zak.global>
2021-12-25 23:10:26 +0100

    Do not require trailing slash

--- packages/fantasion-backend/fantasion_api/urls.py
+++ packages/fantasion-backend/fantasion_api/urls.py
@@ -2,8 +2,16 @@ from django.urls import include, path
 from rest_framework import routers
 
 from fantasion_locations import api as locations
+from fantasion_expeditions import api as expeditions
 
-router = routers.DefaultRouter()
+
+class NoSlashRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.trailing_slash = ''
+
+
+router = NoSlashRouter()
 router.register(
     r'countries',
     locations.CountryCollection,
