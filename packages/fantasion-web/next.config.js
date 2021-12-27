5d80826b0e7561f4a49d7e2fb7e2a6495265c9a4
Pavel Žák <pavel@zak.global>
2021-12-27 15:37:26 +0100

    Initialize locales

--- packages/fantasion-web/next.config.js
+++ packages/fantasion-web/next.config.js
@@ -1,7 +1,7 @@
+const { i18n } = require('./next-i18next.config.js')
+
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    concurrentFeatures: true,
-    reactRoot: true,
-  }
+  i18n,
+  trailingSlash: true,
 }
