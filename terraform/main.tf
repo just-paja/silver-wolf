0bdef46df4ada957711138785fdc4c0abd176c84
Pavel Žák <pavel@zak.global>
2021-12-29 17:33:06 +0100

    Build docker with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,24 @@
+locals {
+  root_dir = abspath("../")
+}
+
 provider "google" {
   project = var.project
   region = var.region
+}
+
+module "backend_docker" {
+  name = "fantasion-backend"
+  path = "${local.root_dir}/packages/fantasion-backend"
+  project_version = "0.1.0"
+  source = "./modules/docker"
+}
+
+module "backend_cloudrun" {
+  name = "fantasion-backend"
+  project = var.project
+  region = var.region
+  source = "./modules/cloudrun"
 }
 
 module "web_cf" {
