0de036899267b153dcec0fb2fb7ba90ba681b389
Pavel Žák <pavel@zak.global>
2021-12-29 22:38:19 +0100

    Deploy to cloud run

---
+++ terraform/modules/cloudrun/main.tf
@@ -0,0 +1,58 @@
+locals {
+  default_envs = [
+    {
+      name = "GCP_PROJECT"
+      value = var.project
+    }
+  ]
+}
+
+resource "google_project_service" "cf" {
+  project = var.project
+  service = "run.googleapis.com"
+}
+
+resource "google_cloud_run_service" "service" {
+  name = "${var.name}-${var.protocol}"
+  location = var.region
+  
+  template {
+    spec {
+      containers {
+        image = var.image_url
+
+        dynamic "env" {
+          for_each = concat(local.default_envs, var.envs)
+          content {
+            name = env.value.name
+            value = env.value.value
+          }
+        }
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "1"
+      }
+    }
+  }
+
+  autogenerate_revision_name = true
+}
+
+data "google_iam_policy" "noauth" {
+  binding { 
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth_policy" {
+  count = var.auth ? 0 : 1
+  location = google_cloud_run_service.service.location
+  service = google_cloud_run_service.service.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
--- terraform/modules/web_cf/main.tf
+++ terraform/modules/web_cf/main.tf
@@ -12,6 +12,8 @@ data "archive_file" "source" {
 resource "google_storage_bucket" "bucket" {
   name = "${var.project}-artifacts"
   location = var.location
+  force_destroy = true
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket_object" "zip" {
