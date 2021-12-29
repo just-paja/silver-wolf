92aecc9d76fff5458565d5774829a52f16ca55df
Pavel Žák <pavel@zak.global>
2021-12-29 20:30:23 +0100

    Deliver image to container registry

--- terraform/modules/docker/main.tf
+++ terraform/modules/docker/main.tf
@@ -1,7 +1,47 @@
-resource "docker_image" "image" {
-  name = var.name
+locals {
+  tag = "${var.name}:${var.project_version}"
+  repo = "eu.gcr.io"
+  actor = "docker@${var.project}.iam.gserviceaccount.com"
+  root = "terraform@${var.project}.iam.gserviceaccount.com"
+}
+
+resource "google_project_service" "container_registry" {
+  service = "containerregistry.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iamcredentials" {
+  service = "iamcredentials.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy = false
+}
+
+resource "google_service_account_iam_binding" "token-creator-iam" {
+    service_account_id = "projects/-/serviceAccounts/${local.actor}"
+    role = "roles/iam.serviceAccountTokenCreator"
+    members = ["serviceAccount:${local.root}"]
+}
+
+data "google_service_account_access_token" "repo" {
+  target_service_account = "${local.actor}"
+  scopes = ["cloud-platform"]
+  lifetime = "300s"
+  delegates = ["projects/-/serviceAccounts/${local.root}"]
+}
+
+provider "docker" {
+  registry_auth {
+    address = local.repo
+    username = "oauth2accesstoken"
+    password = data.google_service_account_access_token.repo.access_token
+  }
+}
+
+resource "docker_registry_image" "image" {
+  name = "${local.repo}/${var.project}/${local.tag}"
+  insecure_skip_verify = true
   build {
-    path = var.path
-    tag = ["${var.name}:${var.project_version}"]
+    context = var.path
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,11 @@ variable "project" {
   type = string
 }
 
+variable "location" {
+  type = string
+  default = "EU"
+}
+
 variable "region" {
   type = string
   default = "europe-west3"
@@ -10,4 +15,8 @@ variable "region" {
 variable "node_runtime" {
   type = string
   default = "nodejs16"
+}
+
+variable "google_credentials" {
+  type = string
 }
