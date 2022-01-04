a4db753133814c971f8af74c73901fada4d877d1
Pavel Žák <pavel@zak.global>
2022-01-05 00:55:07 +0100

    Fix linting issues

--- packages/fantasion-backend/fantasion_generics/storages.py
+++ packages/fantasion-backend/fantasion_generics/storages.py
@@ -3,14 +3,17 @@ from django.utils.functional import LazyObject
 from django.utils.module_loading import import_string
 from storages.backends.gcloud import GoogleCloudStorage
 
-MediaStorage = lambda: GoogleCloudStorage(location='media')
-StaticStorage = lambda: GoogleCloudStorage(location='static')
+
+def MediaStorage():
+    return GoogleCloudStorage(location='media')
+
+
+def StaticStorage():
+    return GoogleCloudStorage(location='static')
+
 
 def get_private_storage_class(import_path=None):
-    return import_string(
-        import_path or
-        settings.PRIVATE_FILE_STORAGE
-    )
+    return import_string(import_path or settings.PRIVATE_FILE_STORAGE)
 
 
 class PrivateStorage(LazyObject):
