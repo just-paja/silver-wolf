a2263632d1b4608743067b7e357079acdeadf30d
Pavel Žák <pavel@zak.global>
2022-01-09 04:35:39 +0100

    Use anonymous cross origin

--- packages/fantasion-web/pages/_document.js
+++ packages/fantasion-web/pages/_document.js
@@ -6,7 +6,11 @@ class FantasionDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
