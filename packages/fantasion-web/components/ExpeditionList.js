b155618a45e368b967e00be2e3933b1f450130f2
Pavel Žák <pavel@zak.global>
2022-01-09 14:40:29 +0100

    Increase card margin

--- packages/fantasion-web/components/ExpeditionList.js
+++ packages/fantasion-web/components/ExpeditionList.js
@@ -2,7 +2,7 @@ import Card from 'react-bootstrap/Card'
 
 const Expedition = ({ expedition }) => {
   return (
-    <Card>
+    <Card className="mt-1">
       <Card.Body>
         <Card.Title>{expedition.title}</Card.Title>
         <Card.Text>{expedition.description}</Card.Text>
