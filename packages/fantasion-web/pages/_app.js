08665298e8526159d41464a17eb8015853403919
Pavel Žák <pavel@zak.global>
2022-01-09 16:03:55 +0100

    Integrate Google Tag Manager

--- packages/fantasion-web/pages/_app.js
+++ packages/fantasion-web/pages/_app.js
@@ -1,12 +1,14 @@
 import { appWithTranslation } from 'next-i18next'
-import { MetaBase, MetaPage } from '../components/meta'
 import { Footer } from '../components/Footer'
+import { GoogleTagManager } from '../components/tracking'
+import { MetaBase, MetaPage } from '../components/meta'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }) => (
   <>
+    <GoogleTagManager />
     <MetaBase />
     <MetaPage title="Fantasion" />
     <Component {...pageProps} />
