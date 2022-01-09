52a4c62f3f5ddce648bd67feb50d4e3a9b292909
Pavel Žák <pavel@zak.global>
2022-01-09 15:12:07 +0100

    Communicate using camelCase

--- packages/fantasion-backend/fantasion/settings.py
+++ packages/fantasion-backend/fantasion/settings.py
@@ -158,7 +158,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':
     ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
     'PAGE_SIZE':
-    20
+    20,
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
 }
 
 CORS_ORIGIN_WHITELIST = (
