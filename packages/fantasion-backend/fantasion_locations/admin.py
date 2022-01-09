ec011f5c9049d9c530e8f89ecb7b8f0ca1c9523b
Pavel Žák <pavel@zak.global>
2022-01-09 18:03:07 +0100

    Filter locations

--- packages/fantasion-backend/fantasion_locations/admin.py
+++ packages/fantasion-backend/fantasion_locations/admin.py
@@ -5,8 +5,32 @@ from . import models
 
 class CountryAdmin(BaseAdmin):
     model = models.Country
-    list_display = ('name',)
+    list_display = ('name', )
 
 
 class LocationAdmin(BaseAdmin):
     model = models.Location
+    list_display = (
+        'name',
+        'city',
+        'street_and_number',
+        'postal_code',
+        'coordinates',
+    )
+    list_filter = ('country', )
+
+    def street_and_number(self, inst):
+        if not inst.street:
+            return None
+        return "{street} {number}".format(
+            street=inst.street,
+            number=inst.street_number,
+        )
+
+    def coordinates(self, inst):
+        if not inst.lat:
+            return None
+        return "{lat}, {lng}".format(
+            lat=inst.lat,
+            lng=inst.lng,
+        )
