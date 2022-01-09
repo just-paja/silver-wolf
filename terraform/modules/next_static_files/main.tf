3fde8117672c085a6cd5b66f50c1fc7302c40e88
Pavel Žák <pavel@zak.global>
2022-01-09 13:28:05 +0100

    Replace static files with each version

--- terraform/modules/next_static_files/main.tf
+++ terraform/modules/next_static_files/main.tf
@@ -1,4 +1,12 @@
+locals {
+  npm = jsondecode(file("${var.path}/package.json"))
+}
+
 resource "null_resource" "static_files" {
+  triggers = {
+    always_run = local.npm.version
+  }
+
   provisioner "local-exec" {
     working_dir = var.path
     environment = {
