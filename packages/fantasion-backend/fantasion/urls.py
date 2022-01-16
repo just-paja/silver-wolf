44754d626835d8e2d293eaf6869f1c3e741270ee
Pavel Žák <pavel@zak.global>
2022-01-16 11:36:46 +0100

    Serve media files locally

--- packages/fantasion-backend/fantasion/urls.py
+++ packages/fantasion-backend/fantasion/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import include, path
 
 from .admin import CONTENT_ADMIN
@@ -6,3 +8,9 @@ urlpatterns = [
     path('api/v1/', include('fantasion_api.urls')),
     path('admin/', CONTENT_ADMIN.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
