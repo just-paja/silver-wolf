b678a972b6718a7c83e4c6773a1d314b562986b9
Pavel Žák <pavel@zak.global>
2022-01-16 11:37:59 +0100

    Prewarm all image thumbs on upload

--- packages/fantasion-backend/fantasion_generics/photos.py
+++ packages/fantasion-backend/fantasion_generics/photos.py
@@ -1,6 +1,10 @@
-from django.db.models import Model
+from django.conf import settings
+from django.db.models import Model, signals
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
+
 from versatileimagefield.fields import VersatileImageField
+from versatileimagefield.image_warmer import VersatileImageFieldWarmer
 
 from .upload_path import get_upload_path
 from .storages import private_storage
@@ -35,3 +39,30 @@ class PrivatePhotoModel(Model):
         verbose_name=_('Image file'),
         width_field='width',
     )
+
+
+warm = (LocalPhotoModel, PrivatePhotoModel)
+
+
+def warm_model(model, inst):
+    keyset = settings.VERSATILEIMAGEFIELD_RENDITION_KEY_SETS
+    for field in model._meta.fields:
+        if isinstance(field, VersatileImageField):
+            for rendition_set in keyset:
+                warm_attr(inst, field.name, rendition_set)
+
+
+def warm_attr(inst, attr, rendition_set):
+    warmer = VersatileImageFieldWarmer(
+        instance_or_queryset=inst,
+        rendition_key_set=rendition_set,
+        image_attr=attr
+    )
+    warmer.warm()
+
+
+@receiver(signals.post_save)
+def warm_images(sender, instance, **kwargs):
+    for model in warm:
+        if issubclass(sender, model):
+            warm_model(model, instance)
