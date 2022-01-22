5482ff531e622c208ef3f812ccc68486f23d340f
Pavel Žák <pavel@zak.global>
2022-01-22 23:47:29 +0100

    Rotate flavour texts on the homepage

--- packages/fantasion-web/components/home.js
+++ packages/fantasion-web/components/home.js
@@ -3,22 +3,64 @@ import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
+import { useEffect, useState, useRef } from 'react'
 import { useTranslation } from 'next-i18next'
 
 import styles from './home.module.scss'
 
+const FLAVOUR_TEXT_TTL = 16000
+
+const getRandomIndex = (items) => Math.floor(Math.random() * items.length)
+
+const getRandomFilteredIndex = (items, currentIndex) => {
+  // Oh my glob. There has to be a better way to do this. But time is mana.
+  const baseArray = Object.keys(items)
+    .map((key) => parseInt(key, 10))
+    .filter((key) => key !== currentIndex)
+  if (baseArray.length === 0) {
+    return null
+  }
+  return baseArray[getRandomIndex(baseArray)]
+}
+
 const FlavourText = ({ quoteOwner, text, ...props }) => {
+  const [height, setHeight] = useState(32)
+  const lineRef = useRef()
+
+  useEffect(() => {
+    if (lineRef.current) {
+      setHeight(lineRef.current.clientHeight)
+    }
+  }, [quoteOwner, text])
+
   return (
-    <blockquote {...props}>
-      <span>&quot;{text}&quot;</span>
-      {' -'}&nbsp;
-      <span>{quoteOwner}</span>
+    <blockquote {...props} style={{ height }}>
+      <span className={styles.flavourLine} ref={lineRef}>
+        <span>&quot;{text}&quot;</span>
+        {' -'}&nbsp;
+        <span>{quoteOwner}</span>
+      </span>
     </blockquote>
   )
 }
 
 const FlavourTextCarousel = ({ flavourTexts, ...props }) => {
-  const flavour = flavourTexts[0]
+  const [flavourIndex, setFlavourIndex] = useState(0)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const nextIndex = getRandomFilteredIndex(flavourTexts, flavourIndex)
+      if (nextIndex !== null) {
+        setFlavourIndex(nextIndex)
+      }
+    }, FLAVOUR_TEXT_TTL)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  })
+
+  const flavour = flavourTexts[flavourIndex]
   if (!flavour) {
     return null
   }
