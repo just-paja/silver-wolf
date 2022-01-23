d306ba568e552b7d32320e2af2439a884f27f2f5
Pavel Žák <pavel@zak.global>
2022-01-23 18:18:41 +0100

    Search FAQ

--- packages/fantasion-web/components/faq.js
+++ packages/fantasion-web/components/faq.js
@@ -2,6 +2,8 @@ import Markdown from 'react-markdown'
 
 import { ControlledForm, Input } from './forms'
 import { FormProvider, useForm } from 'react-hook-form'
+import { Index } from 'flexsearch'
+import { useMemo } from 'react'
 import { useTranslation } from 'next-i18next'
 
 const Faq = ({ question, detailedAnswer, shortAnswer }) => {
@@ -29,29 +31,48 @@ const FaqFilter = () => {
   )
 }
 
-const includes = (str, query) => str.includes(query)
+const createIndex = (language, faqs) => {
+  const index = new Index({
+    charset: 'latin:extra',
+    language,
+  })
+  for (const faq of faqs) {
+    index.add(
+      faq.id,
+      `${faq.question} ${faq.shortAnswer} ${faq.detailedAnswer}`
+    )
+  }
+  return index
+}
+
+const sortByMatch = (matchIds) => (a, b) =>
+  matchIds ? matchIds.indexOf(a.id) - matchIds.indexOf(b.id) : 0
 
 export const Faqs = ({ faqs }) => {
+  const { i18n } = useTranslation()
   const form = useForm()
   const q = form.watch('q')
-  const filterByQuery = (query) => (item) =>
-    !query ||
-    includes(item.question, query) ||
-    includes(item.shortAnswer, query) ||
-    includes(item.detailedAnswer, query)
+  const index = useMemo(
+    () => createIndex(i18n.language, faqs.results),
+    [i18n, faqs.results]
+  )
+  const matchIds = q ? index.search(q) : null
   return (
     <>
       <FormProvider {...form}>
         <FaqFilter />
       </FormProvider>
       <dl>
-        {faqs.results.filter(filterByQuery(q)).map((faq) => (
-          <Faq
-            question={faq.question}
-            shortAnswer={faq.shortAnswer}
-            key={faq.id}
-          />
-        ))}
+        {faqs.results
+          .filter((faq) => !matchIds || matchIds.includes(faq.id))
+          .sort(sortByMatch(matchIds))
+          .map((faq) => (
+            <Faq
+              question={faq.question}
+              shortAnswer={faq.shortAnswer}
+              key={faq.id}
+            />
+          ))}
       </dl>
     </>
   )
