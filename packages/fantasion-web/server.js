fab8c28ec5ba2fb3ca1de3409fc22fdea8cde5cd
Pavel Žák <pavel@zak.global>
2022-01-31 13:01:23 +0100

    Provide port and hostname just as the warning says

--- packages/fantasion-web/server.js
+++ packages/fantasion-web/server.js
@@ -2,9 +2,10 @@ const { createServer } = require('http')
 
 const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
 const port = process.env.PORT || 3000
+const hostname = process.env.FRONTEND_HOST || 'localhost'
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => handle(req, res)).listen(port, (err) => {
