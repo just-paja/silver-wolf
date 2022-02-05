7919febeb928fe821af54c2b396b2623a86da508
Pavel Žák <pavel@zak.global>
2022-02-05 17:05:10 +0100

    feat: add login link to navbar

--- packages/fantasion-web/components/layout.js
+++ packages/fantasion-web/components/layout.js
@@ -52,12 +52,19 @@ export const SiteNavbar = ({ fixed, sticky }) => {
             <span>{t('fantasion-brand')}</span>
           </Navbar.Brand>
         </Linker>
-        <Navbar.Toggle
-          aria-controls="site-navbar"
-          className={styles.navbarToggle}
-        >
-          <HiMenu />
-        </Navbar.Toggle>
+        <div className="d-flex flex-row">
+          <Nav>
+            <Linker route="login">
+              <Nav.Link>{t('login')}</Nav.Link>
+            </Linker>
+          </Nav>
+          <Navbar.Toggle
+            aria-controls="site-navbar"
+            className={styles.navbarToggle}
+          >
+            <HiMenu />
+          </Navbar.Toggle>
+        </div>
         <Navbar.Collapse id="site-navbar">
           <Nav>
             <Linker route="adventureList">
