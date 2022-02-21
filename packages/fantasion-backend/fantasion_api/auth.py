1dfc5213232564a5a2b6a6f06e5995e87d0a5904
Pavel Žák <pavel@zak.global>
2022-02-21 02:23:40 +0100

    refactor: fix linting issues

--- packages/fantasion-backend/fantasion_api/auth.py
+++ packages/fantasion-backend/fantasion_api/auth.py
@@ -1,8 +1,7 @@
-from rest_framework import authentication
-from rest_framework.authtoken.models import Token
 from rest_framework.authentication import SessionAuthentication
 
 
 class CsrfExemptAuth(SessionAuthentication):
+
     def enforce_csrf(self, request):
         return
