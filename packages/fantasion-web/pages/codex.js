1999c1bc85e1f0546831196e791628b3deb59a8d
Vít Vršník <vrsnivit@gmail.com>
2022-04-05 22:57:03 +0200

    Fix: frontend format document problems solved

--- packages/fantasion-web/pages/codex.js
+++ packages/fantasion-web/pages/codex.js
@@ -1,7 +1,6 @@
 import { createStaticArticlePageGetter } from '../server/articles'
 import { StaticArticlePage } from '../components/articles'
 
-export const getServerSideProps =
-  createStaticArticlePageGetter('codex')
+export const getServerSideProps = createStaticArticlePageGetter('codex')
 
 export default StaticArticlePage
