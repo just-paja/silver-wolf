f0f06eae0b5f54ff0f9a8644cd41b37325541fbf
Vít Vršník <vrsnivit@gmail.com>
2022-04-06 19:49:31 +0200

    feat: bestiary-info from static article

--- packages/fantasion-web/pages/bestiary.js
+++ packages/fantasion-web/pages/bestiary.js
@@ -12,17 +12,18 @@ import { withPageProps } from '../server/props'
 export const getServerSideProps = withPageProps(async ({ fetch }) => ({
   props: {
     monsters: await fetch('/monsters'),
+    bestiaryInfo: await fetch('/static-articles/bestiary-info'),
   },
 }))
 
-const BestiaryPage = ({ monsters }) => {
+const BestiaryPage = ({ monsters, bestiaryInfo }) => {
   const { t } = useTranslation()
   return (
     <GenericPage>
       <MetaPage title={t('bestiary')} description={t('bestiary-description')} />
       <Container>
         <Heading>{t('bestiary')}</Heading>
-        <ArticleBody text={t('bestiary-description')} />
+        <ArticleBody text={bestiaryInfo.text} />
         <MonsterList monsters={monsters.results} />
       </Container>
     </GenericPage>
