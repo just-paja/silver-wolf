1684db54cd0cffd43936c8b6b2b4cfb43bcb68f3
Pavel Žák <pavel@zak.global>
2022-04-11 15:46:56 +0200

    fix: pass money to amount

--- packages/fantasion-backend/fantasion_banking/driver_fio.py
+++ packages/fantasion-backend/fantasion_banking/driver_fio.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from djmoney.money import Money
 from django.utils import timezone
 from fiobank import FioBank
 
@@ -17,30 +18,34 @@ def sync(account, scrape, *args, **kwargs):
     if last:
         gen = client.last(from_id=last.ident)
     else:
-        gen = client.last()
+        gen = client.last(from_date='2022-04-01')
+
+    print(last, flush=True)
+    print(gen, flush=True)
 
     for line in gen:
+        defaults = {
+            'amount': Money(get(line, 'amount'), get(line, 'currency')),
+            'constant_symbol': get(line, 'constant_symbol'),
+            'message': get(line, 'recipient_message'),
+            'received_at': timezone.make_aware(
+                datetime.combine(
+                    get(line, 'date'),
+                    datetime.min.time(),
+                ),
+            ),
+            'sender_account_number': get(line, 'account_number'),
+            'sender_bank': get(line, 'bank_code'),
+            'sender_bic': get(line, 'bic'),
+            'sender_iban': get(line, 'iban'),
+            'specific_symbol': get(line, 'specific_symbol'),
+            'user_identification': get(line, 'user_identification'),
+            'variable_symbol': get(line, 'variable_symbol'),
+            'scrape': scrape,
+        }
+
         Statement.objects.get_or_create(
             ident=line['transaction_id'],
             account=account,
-            defaults={
-                'amount': get(line, 'amount'),
-                'constant_symbol': get(line, 'constant_symbol'),
-                'currency': get(line, 'currency'),
-                'message': get(line, 'recipient_message'),
-                'received_at': timezone.make_aware(
-                    datetime.combine(
-                        get(line, 'date'),
-                        datetime.min.time(),
-                    ),
-                ),
-                'sender_account_number': get(line, 'account_number'),
-                'sender_bank': get(line, 'bank_code'),
-                'sender_bic': get(line, 'bic'),
-                'sender_iban': get(line, 'iban'),
-                'specific_symbol': get(line, 'specific_symbol'),
-                'user_identification': get(line, 'user_identification'),
-                'variable_symbol': get(line, 'variable_symbol'),
-                'scrape': scrape,
-            },
+            defaults=defaults,
         )
